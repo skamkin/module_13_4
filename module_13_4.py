ec7f70889482ccb69956d63c96d37a6afa08a235
skamkin <doc.kamkin@gmail.com>
2024-10-20 20:53:19 +0500

    Update module_13_4.py

--- module_13_4.py
+++ module_13_4.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, executor
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-api = '7828531761:AAHF0aGlinpYpGL6JL0ogkCfAePmBLKZDGg'
+api = '78'
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
